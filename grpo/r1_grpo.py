2ed34e6a24075d8210d071a099e3411a0a7ce4d9
Wing Lian <wing@axolotl.ai>
2025-02-04 09:00:31 -0500

    updates to make it work

--- grpo/r1_grpo.py
+++ grpo/r1_grpo.py
@@ -1,30 +1,10 @@
-# Copied from https://github.com/huggingface/open-r1/blob/main/src/open_r1/grpo.py
-#
-# Copyright 2025 The HuggingFace Team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import re
-
-from latex2sympy2_extended import NormalizationConfig
-from math_verify import LatexExtractionConfig, parse, verify
-
 
 SYSTEM_PROMPT = (
     "A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant "
     "first thinks about the reasoning process in the mind and then provides the user with the answer. The reasoning "
-    "process and answer are enclosed within <think> </think> and <answer> </answer> tags with \\boxed{}, respectively, i.e., "
-    "<think> reasoning process here </think><answer> \\boxed{answer here} </answer>"
+    "process and answer are enclosed within <think> </think> and <answer> </answer>, respectively, i.e., "
+    "<think> reasoning process here </think><answer> answer here </answer>"
 )
 
 
@@ -45,55 +25,29 @@ def gsm8k_transform(cfg, *args, **kwargs):
                 {"role": "system", "content": SYSTEM_PROMPT},
                 {"role": "user", "content": example["text"]},
             ],
-            "solution": "\\boxed{" + example["label"] + "}"
+            "solution": example["label"],
         }
     return transform_fn, {"remove_columns": ["text", "label"]}
 
 
-def accuracy_reward(completions, solution, **kwargs):
-    """Reward function that checks if the completion is the same as the ground truth."""
+def correct_answer_reward(completions, solution, **kwargs):
+    # look for the correct answer as the last numeric match in the completion
     contents = [completion[0]["content"] for completion in completions]
     rewards = []
     for content, sol in zip(contents, solution):
-        gold_parsed = parse(sol, extraction_mode="first_match", extraction_config=[LatexExtractionConfig()])
-        if len(gold_parsed) != 0:
-            # We require the answer to be provided in correct latex (no malformed operators)
-            answer_parsed = parse(
-                content,
-                extraction_config=[
-                    LatexExtractionConfig(
-                        normalization_config=NormalizationConfig(
-                            nits=False,
-                            malformed_operators=False,
-                            basic_latex=True,
-                            equations=True,
-                            boxed=True,
-                            units=True,
-                        ),
-                        # Ensures that boxed is tried first
-                        boxed_match_priority=0,
-                        try_extract_without_anchor=False,
-                    )
-                ],
-                extraction_mode="first_match",
-            )
-            # Reward 1 if the content is the same as the ground truth, 0 otherwise
-            try:
-                reward = float(verify(answer_parsed, gold_parsed))
-            except TypeError:
-                reward = 0.0
-        else:
-            # If the gold solution is not parseable, we reward 1 to skip this example
-            reward = 1.0
-            print("Failed to parse gold solution: ", sol)
+        # reverse the string and search from the end for numeric matches
+        matches = re.search(r"([0-9]+\$-?)|([0-9]+\-?)", content[::-1])
+        reward = 0.0
+        if matches:
+            res = matches[0][::-1] if matches[0] else matches[1][::-1]
+            if res.strip() == str(sol):
+                reward = 1.0
         rewards.append(reward)
-
     return rewards
-
 
 def format_reward(completions, **kwargs):
     """Reward function that checks if the completion has a specific format."""
-    pattern = r"^<think>.*?</think>\s*<answer>.*?</answer>$"
+    pattern = r"^<think>.*?</think>\s*<answer>.*?</answer>"
     completion_contents = [completion[0]["content"] for completion in completions]
     matches = [re.match(pattern, content) for content in completion_contents]
     return [1.0 if match else 0.0 for match in matches]
